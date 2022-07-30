82cdd557d0e680cd1e9fafd33a3fd7f1f0056b78
Paul Taylor <paul@whammydiver.com>
2022-07-30 10:37:08 -0400

    join 3 tables query

--- 2_queries_joins/totalAssignmentTime.sql
+++ 2_queries_joins/totalAssignmentTime.sql
@@ -1,4 +1,5 @@
-SELECT sum(assignments.duration)
-  FROM assignments
-  JOIN cohorts ON assignments.name = cohorts.name
-  WHERE cohorts.id = 'FEB12';
+SELECT SUM(assignment_submissions.duration) AS total_duration
+  FROM assignment_submissions
+  JOIN students ON students.id = assignment_submissions.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = 'FEB12';
